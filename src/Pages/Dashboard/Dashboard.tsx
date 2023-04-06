331d6d49abe08356c3664bd1fc09d2d02217fe52
nicholas kipkoech <nickey968@gmail.com>
2023-04-06 17:09:53 +0300

    chore:fix bugs

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,33 +1,38 @@
 import React, { useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
 import { ContactServiceClient } from "../../generated/ContactsServiceClientPb";
-import { Contact } from "../../generated/contacts_pb";
+import { Empty } from "../../generated/contacts_pb";
 
 import Cookies from "js-cookie";
 
 const Dashboard = () => {
   const token = Cookies.get("accessToken");
-  const metadata = { Authorization: `Bearer ${token}` };
+
+  const metadata = { authorization: `${token}` };
 
   useEffect(() => {
-    const contactService = new ContactServiceClient("http://localhost:8080");
-    const getContactsList = () => {
-      const contacts = new Contact();
-      contactService.getContacts(contacts, metadata, (err, response) => {
-        if (err) {
-          console.log(err);
-          return;
-        } else {
-          console.log(response.getContactsList);
-        }
-      });
-    };
-    getContactsList();
+    const contactService = new ContactServiceClient(
+      "http://localhost:8080",
+      null
+    );
+
+    const contacts = new Empty();
+    contactService.getContacts(contacts, metadata, (err, response) => {
+      if (err) {
+        console.log("error fetching contacts");
+        return;
+      } else {
+        console.log(response.getContactsList());
+      }
+    });
   }, []);
 
   return (
     <div className="w-full justify-center">
       <Navbar />
+      <div>
+        <table></table>
+      </div>
     </div>
   );
 };
