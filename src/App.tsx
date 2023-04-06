fe6033d205a7ac2aacb73c500be0550806d3114e
nicholas kipkoech <nickey968@gmail.com>
2023-04-06 09:15:13 +0300

    configure store and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Login from './Pages/Auth/Login';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./Pages/Auth/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route  path='/' element={<Login/>}/>
+          <Route path="/" element={<Login />} />
         </Routes>
       </Router>
     </div>
