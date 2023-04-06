2502505259778da954677890a26bef2d3b94621f
nicholas kipkoech <nickey968@gmail.com>
2023-04-06 11:08:23 +0300

    check if not loggged it

--- src/Pages/Dashboard/ProtectedRoute.tsx
+++ src/Pages/Dashboard/ProtectedRoute.tsx
@@ -1,21 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
-interface AuthState {
-  isAuthenticated: boolean;
-  accessToken: string | null;
-  refreshToken: string | null;
-  username: string | null;
-  userId: string | null;
-}
 interface ProtectedRouteProps {
   children: any;
 }
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const authState: any = useSelector<AuthState>((state) => state);
+  //get authState from the browserStorage to persists the logged in state
+  const authState: any = localStorage.getItem("authState");
   const location = useLocation();
-  if (authState.isAuthenticated === false) {
+  //redirect to login if no authState
+  if (!authState) {
     return <Navigate to="/" state={{ from: location }} replace />;
   }
 
