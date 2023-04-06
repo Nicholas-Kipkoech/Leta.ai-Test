d476de5ecb85c95c92cd8a8d63938c47db887dbf
nicholas kipkoech <nickey968@gmail.com>
2023-04-06 12:02:54 +0300

    check if tokens then persist login

--- src/Pages/Dashboard/ProtectedRoute.tsx
+++ src/Pages/Dashboard/ProtectedRoute.tsx
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
@@ -6,7 +7,7 @@ interface ProtectedRouteProps {
 }
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   //get authState-accessToken from the browserStorage to persists the logged in state
-  const authState: any = localStorage.getItem("accessToken");
+  const authState: any = Cookies.get("accessToken");
   const location = useLocation();
   //redirect to login if no authState
   if (!authState) {
