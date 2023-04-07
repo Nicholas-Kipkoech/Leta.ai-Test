87a134887b2847afd0ddcc6eed47cf71dbfaea49
nicholas kipkoech <nickey968@gmail.com>
2023-04-07 07:50:07 +0300

    dispact function for pushing payload to the array

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -17,12 +17,46 @@ import {
   StyledTableCell,
   StyledTableRow,
 } from "../../reusableComponents/StyledComponents";
+import { useDispatch, useSelector } from "react-redux";
+import { Contact, addContact } from "../../Features/Contacts/ContactsReducer";
+import { nanoid } from "@reduxjs/toolkit";
+import { ToastContainer, toast } from "react-toastify";
 
 const Dashboard = () => {
   const [visible, setVisible] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [formError, setFormError]: any = useState("");
+
+  //dispatch function
+
+  const dispatch = useDispatch();
+
+  const contactData: Contact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  //submit the data from the form
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //validate input
+    if (!name || !email || !phone) {
+      setFormError("Please fill in all the  fields.");
+      return;
+    }
+
+    dispatch(addContact(contactData));
+    setFormError(null);
+    setEmail("");
+    setPhone("");
+    setName("");
+  };
+
+  const { contacts } = useSelector((state: any) => state.contacts);
 
   return (
     <div className="w-full mr-60 ml-60 justify-center ">
@@ -38,10 +72,11 @@ const Dashboard = () => {
         <CustomModal
           ModalVisible={visible}
           handleCancel={() => setVisible(false)}
-          handleOk={() => setVisible(false)}
+          handleOk={handleSubmit}
           Title="Add contact"
           content={
             <>
+              <div className="text-red-600">{formError && formError}</div>
               <CustomInput
                 value={name}
                 onChange={(e) => setName(e.target.value)}
@@ -80,23 +115,26 @@ const Dashboard = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <StyledTableRow>
-              <StyledTableCell component="th" scope="row">
-                hello
-              </StyledTableCell>
-              <StyledTableCell align="right">hello</StyledTableCell>
-              <StyledTableCell align="right">hello</StyledTableCell>
-              <StyledTableCell align="right">
-                <ButtonGroup
-                  variant="contained"
-                  aria-label="contained primary button group"
-                  className="gap-5"
-                >
-                  <Button color="primary">Edit</Button>
-                  <Button color="error">Delete</Button>
-                </ButtonGroup>
-              </StyledTableCell>
-            </StyledTableRow>
+            {contacts.map((contact: any) => (
+              <StyledTableRow>
+                <StyledTableCell component="th" scope="row" key={contact.id}>
+                  {contact.name}
+                </StyledTableCell>
+                <StyledTableCell align="right">{contact.phone}</StyledTableCell>
+                <StyledTableCell align="right">{contact.email}</StyledTableCell>
+
+                <StyledTableCell align="right">
+                  <ButtonGroup
+                    variant="contained"
+                    aria-label="contained primary button group"
+                    className="gap-5"
+                  >
+                    <Button color="primary">Edit</Button>
+                    <Button color="error">Delete</Button>
+                  </ButtonGroup>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
