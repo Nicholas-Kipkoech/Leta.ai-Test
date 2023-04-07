e3dab598125a7082071348096a35ebbdc6cd26ab
nicholas kipkoech <nickey968@gmail.com>
2023-04-07 16:10:35 +0300

    chore:working on create contact

--- src/Pages/Dashboard/Edit.tsx
+++ src/Pages/Dashboard/Edit.tsx
@@ -10,43 +10,53 @@ import {
 
 import { useNavigate, useParams } from "react-router-dom";
 import { nanoid } from "@reduxjs/toolkit";
+import { ContactServiceClient } from "../../generated/ContactsServiceClientPb";
+import { Contact } from "../../generated/contacts_pb";
+import { toast } from "react-toastify";
+import Cookies from "js-cookie";
 
 const Edit = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { id } = useParams();
-  const user = useSelector((state: any) => state.contacts.selectedContact);
-
-  useEffect(() => {
-    dispatch(getContact(id));
-  }, [id]);
-
-  useEffect(() => {
-    if (user) {
-      setName(user.name);
-      setPhone(user.phone);
-      setEmail(user.email);
-    }
-  }, [user]);
+  const { id }: any = useParams();
 
   const handleSubmit = () => {
-    dispatch(editContact({ id: user.id, name, phone, email }));
-    setError("");
+    const token = Cookies.get("accessToken");
 
-    return navigate("/dashboard");
+    const metadata = { authorization: `${token}` };
+
+    const client = new ContactServiceClient("http://localhost:8080");
+    const request = new Contact();
+    request.setId(id);
+    request.setName(name);
+    request.setEmail(email);
+    request.setPhone(phone);
+    try {
+      client.updateContact(request, metadata, (err: any, response) => {
+        if (err) {
+          toast(err);
+        }
+        if (response === null) {
+          toast("All fields are required");
+        } else {
+          dispatch(editContact({ id, name, email, phone }));
+          return navigate("/dashboard");
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="w-full  mr-60 ml-60 justify-center  ">
       <Navbar />
       <form>
-        {error && <h2 className="text-red-600 justify-center">{error}</h2>}
         <div className=" flex gap-6 border-gray-600  justify-center">
           <CustomInput
             value={name}
