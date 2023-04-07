32f5d5db000aca9543db78278d321e7b0c552f1a
nicholas kipkoech <nickey968@gmail.com>
2023-04-07 21:55:15 +0300

    add services for accessToken

---
+++ src/armory/accessToken.tsx
@@ -0,0 +1,59 @@
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
+import { refreshAccessToken } from "./refreshToken";
+
+// This function checks if the access token is still valid
+export const isAccessTokenValid = (): boolean => {
+  const accessToken = Cookies.get("accessToken");
+  if (!accessToken) {
+    return false;
+  }
+
+  try {
+    const decodedToken: any = jwt_decode(accessToken);
+    const currentTime = Date.now() / 1000; // convert to seconds
+    if (decodedToken.exp < currentTime) {
+      // access token has expired
+      return false;
+    }
+    return true; // access token is still valid
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+// This function gets a new access token using the refresh token
+export const getNewAccessToken = async (): Promise<string | null> => {
+  const refreshToken = Cookies.get("refreshToken");
+  if (!refreshToken) {
+    return null;
+  }
+
+  try {
+    const response: any = await refreshAccessToken(refreshToken);
+    console.log(response);
+    const accessToken = response;
+    Cookies.set("accessToken", accessToken);
+    return accessToken;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+// This function is called before making an API call to check if the access token is still valid and get a new one if necessary
+export const getTokenAndRefreshIfNeeded = async (): Promise<string | any> => {
+  if (isAccessTokenValid()) {
+    // access token is still valid
+    return Cookies.get("accessToken");
+  }
+
+  // access token has expired, try to get a new one
+  const newAccessToken = await getNewAccessToken();
+  if (newAccessToken) {
+    return newAccessToken;
+  }
+
+  return null; // failed to get a new access token
+};
---
+++ src/armory/refreshToken.tsx
@@ -0,0 +1,25 @@
+import { AuthServiceClient } from "../generated/AuthServiceClientPb";
+import { RefreshAccessTokenRequest } from "../generated/auth_pb";
+
+export const refreshAccessToken = async (refreshToken: string) => {
+  const authService = new AuthServiceClient("http://localhost:8080");
+
+  const refreshAccessTokenRequest = new RefreshAccessTokenRequest();
+  refreshAccessTokenRequest.setRefreshtoken(refreshToken);
+
+  return new Promise((resolve, reject) => {
+    authService.refreshAccessToken(
+      refreshAccessTokenRequest,
+      {},
+      (err, response) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          const accessToken = response.getAccesstoken();
+          resolve(accessToken);
+        }
+      }
+    );
+  });
+};
