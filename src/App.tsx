27769ba98dd6284120385823da6d8a3f64ba367c
nicholas kipkoech <nickey968@gmail.com>
2023-04-08 10:57:53 +0300

    create refresh flow for tokens'

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
@@ -8,7 +8,36 @@ import ProtectedRoute from "./Pages/Dashboard/ProtectedRoute";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Add from "./Pages/Dashboard/Add";
 import Edit from "./Pages/Dashboard/Edit";
+import { AuthServiceClient } from "./generated/AuthServiceClientPb";
+import Cookies from "js-cookie";
+import { RefreshAccessTokenRequest } from "./generated/auth_pb";
+import decode from "jwt-decode";
+const authClient = new AuthServiceClient("http://localhost:8080");
 function App() {
+  const refreshToken: any = Cookies.get("refreshToken");
+  const accessToken: any = Cookies.get("accessToken");
+
+  useEffect(() => {
+    const decodedToken: any = decode(accessToken);
+    if (
+      decodedToken &&
+      decodedToken.exp &&
+      decodedToken.exp < Date.now() / 1000
+    ) {
+      console.log("token expired");
+      console.log("refreshToken=====", refreshToken);
+      const request = new RefreshAccessTokenRequest();
+      request.setRefreshtoken(refreshToken);
+      authClient.refreshAccessToken(request, {}, (err, response) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const newAccessToken = response.getAccesstoken();
+        Cookies.set("accessToken", newAccessToken);
+      });
+    }
+  }, [refreshToken, accessToken]);
   return (
     <>
       <div className="App">
